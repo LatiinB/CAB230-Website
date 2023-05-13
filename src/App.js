3c6f3dbc0d365187546e1dc0431380da2745a8d6
Bradley Watson <bwats127@gmail.com>
2023-05-13 15:07:40 +1000

    Navbar and Styling Test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './Pages';
+import About from './Pages/about';
+import Blogs from './Pages/blogs';
+import SignUp from './Pages/signup';
+import Contact from './Pages/contact';
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      Main
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+          <React.Fragment>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/blogs' element={<Blogs/>} />
+            <Route path='/sign-up' element={<SignUp/>} />
+          </React.Fragment>
+        </Routes>
+      </React.Fragment>
+    </Router>
   );
 }
-
+  
 export default App;
